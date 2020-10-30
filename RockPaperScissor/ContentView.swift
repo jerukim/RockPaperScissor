b67786da4c793469cab885f748858d97b3d992ab
jeru <mikurej95@gmail.com>
2020-10-30 00:59:35 -0400

    Initial Commit

---
+++ RockPaperScissor/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RockPaperScissor
+//
+//  Created by Jeru Kim on 10/30/20.
+//  Copyright © 2020 Jeru Kim. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
